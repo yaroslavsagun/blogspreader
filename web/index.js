3ee515530a76b9813dec0616a476dffb6634beab
Yaroslav Sagun <yaroslavsagun@Yaroslavs-MacBook-Air.local>
2022-07-27 19:30:09 +0300

    config changes

--- web/index.js
+++ web/index.js
@@ -49,7 +49,7 @@ Shopify.Webhooks.Registry.addHandler("APP_UNINSTALLED", {
 
 // The transactions with Shopify will always be marked as test transactions, unless NODE_ENV is production.
 // See the ensureBilling helper to learn more about billing in this template.
-const BILLING_SETTINGS = {
+const BILLING_SETTINGS = { 
   required: false,
   // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
   // chargeName: "My Shopify One-Time Charge",
